552e69244a530cda9de65ee27d66ff6f5459b0ac
Stephen Kettley <kettleystephen@gmail.com>
2023-09-14 09:33:01 +0200

    finalise functionality

--- index.js
+++ index.js
@@ -15,10 +15,40 @@ async function searchImages() {
   const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
+  if (page === 1) {
+    searchResultsElement.innerHTML = "";
+  }
+
+  const results = data.results;
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResultsElement.appendChild(imageWrapper);
+  });
+
+  page++;
+
+  if (page > 1) {
+    showMoreButtonElement.style.display = "block";
+  }
 }
 
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
+  page = 1;
+  searchImages();
+});
+
+showMoreButtonElement.addEventListener("click", () => {
   searchImages();
 });
